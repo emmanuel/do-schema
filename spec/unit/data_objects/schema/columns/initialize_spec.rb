7f31a76094d846ea094509cd7d05badd010d1b66
Emmanuel Gomez <emmanuel.gomez@gmail.com>
2011-06-25 23:20:15 -0700

    Try nesting specs inside DataObjects::Schema module.

--- spec/unit/data_objects/schema/columns/initialize_spec.rb
+++ spec/unit/data_objects/schema/columns/initialize_spec.rb
@@ -2,42 +2,43 @@ require 'spec_helper'
 require 'data_objects/schema/column'
 require 'spec/unit/data_objects/schema/ordered_set/shared/initialize_spec'
 
-describe 'DataObjects::Schema::ColumnSet#initialize' do
+module DataObjects::Schema
+  describe ColumnSet, '#initialize' do
 
-  context 'when no entries are given' do
+    context 'when no entries are given' do
 
-    subject { DataObjects::Schema::ColumnSet.new }
+      subject { ColumnSet.new }
 
-    it_should_behave_like 'DataObjects::Schema::OrderedSet#initialize when no entries are given'
-
-  end
-
-  context 'when entries are given' do
-
-    subject { DataObjects::Schema::ColumnSet.new(entries) }
-
-    context 'and they do not contain duplicates' do
-
-      let(:entries) { [entry1, entry2]                              }
-
-      let(:entry1)  { DataObjects::Schema::Column.new('entry1', {}) }
-      let(:entry2)  { DataObjects::Schema::Column.new('entry2', {}) }
-
-      it_should_behave_like 'DataObjects::Schema::OrderedSet#initialize when entries are given and they do not contain duplicates'
+      it_should_behave_like 'DataObjects::Schema::OrderedSet#initialize when no entries are given'
 
     end
 
-    context 'and they contain duplicates' do
+    context 'when entries are given' do
 
-      let(:entries) { [entry1, entry2]                              }
+      subject { ColumnSet.new(entries) }
 
-      let(:entry1)  { DataObjects::Schema::Column.new('entry1', {}) }
-      let(:entry2)  { entry1.dup                                    }
+      context 'and they do not contain duplicates' do
 
-      it_should_behave_like 'DataObjects::Schema::OrderedSet#initialize when entries are given and they contain duplicates'
+        let(:entries) { [entry1, entry2]                              }
 
+        let(:entry1)  { Column.new('entry1', {}) }
+        let(:entry2)  { Column.new('entry2', {}) }
+
+        it_should_behave_like 'DataObjects::Schema::OrderedSet#initialize when entries are given and they do not contain duplicates'
+
+      end
+
+      context 'and they contain duplicates' do
+
+        let(:entries) { [entry1, entry2]                              }
+
+        let(:entry1)  { Column.new('entry1', {}) }
+        let(:entry2)  { entry1.dup                                    }
+
+        it_should_behave_like 'DataObjects::Schema::OrderedSet#initialize when entries are given and they contain duplicates'
+
+      end
     end
-  end
 
+  end
 end
-
